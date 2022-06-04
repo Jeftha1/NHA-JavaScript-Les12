b5c6b83e83769e24c5d0d0b80d2647a9ca7ddc1f
Jeftha1 <89483369+Jeftha1@users.noreply.github.com>
2022-06-04 14:13:16 +0200

    Update script12-3.js

--- script12-3.js
+++ script12-3.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$("#btnGetWeather").on('click', function(){
-		const cityUrl = 'https://openweathermap.org/city';
-		const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Emmeloord,nl&APPID=3a3b835ac7c4b0ea6f87072d46fb8dde&units=metric&lang=nl'; 
+		const cityUrl = 'https://openweathermap.org/city/2756139';
+		const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?APPID=3a3b835ac7c4b0ea6f87072d46fb8dde&q=Emmeloord&units=metric&lang=nl'; 
 		$.ajax({
 			url: weatherUrl,
 			succes: function(weather){
-				console.log(weather.id); 
+				console.log(weather); 
 				const divResult = $('#divResult'); 
 				divResult.empty(); 
-				let html = '<strong><a href="' + cityUrl + '/' + weather.id + '" ';
+				let html = '<strong><a href="' + cityUrl + '" ';
 					html += 'target="_blank">' + weather.name;
 					html += ' (' + weather.sys.country + ')</a></strong>'; 
 					html += ', temperatuur: ' + weather.main.temp + ' graden.<br>'; 
